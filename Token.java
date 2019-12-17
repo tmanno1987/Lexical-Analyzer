51c560ba51b1acf76c59ed0f7808138c7c9a1e94
Timothy Manno <38054853+tmanno1987@users.noreply.github.com>
2019-12-16 22:37:03 -0600

    all up to date items as of 12-16-2019
    Lex is still in progress

---
+++ Token.java
@@ -0,0 +1,47 @@
+package Lex;
+
+import java.util.*;
+import java.io.*;
+
+public class Token
+{
+   private String tok;
+   private String cls;
+   
+   public Token()
+   {
+      tok = null;
+      cls = null;
+   }
+   
+   public Token(String tok, String cls)
+   {
+      this.tok = tok;
+      this.cls = cls;
+   }
+   
+   public String getToken()
+   {
+      return tok;
+   }
+   
+   public String getCls()
+   {
+      return cls;
+   }
+   
+   public void setToken(String t)
+   {
+      tok = t;
+   }
+   
+   public void setCls(String c)
+   {
+      cls = c;
+   }
+   
+   public String toString()
+   {
+      return tok + "\t\t" + cls;
+   }
+}
